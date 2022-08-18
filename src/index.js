328056c1ab83f2badecdcbd01a4468511170b95d
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-18 20:04:33 +0200

    aa

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Hello from './Hello';
+import Grafik from './Grafik';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello />
+    <Grafik />
   </React.StrictMode>
 );
 
