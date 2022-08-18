8cbed258bff4a4cf3281c2cd3ea36cbf13e890d2
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-18 20:01:53 +0200

    file name changed and file modified

---
+++ src/Grafik.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Grafik = () => (
+    <div>
+        <div className="countdown">
+            <strong>śniadanie</strong> - 07:00
+        </div>
+        <div className="countdown">
+            <strong>obiad</strong> - 15:00
+        </div>
+    </div>
+);
+
+export default Grafik;
