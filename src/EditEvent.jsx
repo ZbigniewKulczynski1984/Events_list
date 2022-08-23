cef53daee12985362404f313ea012d5413454ead
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-23 12:23:50 +0200

    info added in EditEvent

---
+++ src/EditEvent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditEvent = () => {
+    return (
+        <div className="edit-event">
+            <input type="text" />
+        </div>
+    )
+}
+
+export default EditEvent;
