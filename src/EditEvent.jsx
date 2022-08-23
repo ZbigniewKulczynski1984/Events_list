c04d9a7367ed705e10a31be1d02e1255f80f6e5e
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-23 12:34:32 +0200

    info added to Editevent.jsx

--- src/EditEvent.jsx
+++ src/EditEvent.jsx
@@ -7,6 +7,16 @@ const EditEvent = () => {
                 <label htmlFor="name">name</label>
                 <input type="text" id="name" name="name" />
             </div>
+            <div className="edit-event__input-group">
+                <label htmlFor="hour">hour</label>
+                <input type="tel" id="hour" name="hour" />
+            </div>
+            <div className="edit-event__input-group">
+                <label htmlFor="minute">minute</label>
+                <input type="tel" id="minute" name="minute" />
+            </div>
+            <button>OK</button>
+            <button>Cancel</button>
             
         </div>
     );
